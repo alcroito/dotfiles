38cb612b5b08e4153fe8e03b704388798659c4f7
alcroito <alcroito.code@pm.me>
2025-03-10 16:28:01 +0100

    Install powershell and winget via choco

--- install.ps1
+++ install.ps1
@@ -5,13 +5,23 @@
 # https://github.com/microsoft/winget-cli/discussions/3964
 # December 2023
 # https://github.com/microsoft/winget-cli/releases/download/v1.6.3482/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-$url = "https://github.com/microsoft/winget-cli/releases/download/v1.6.3482/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-$outpath = "winget.msixbundle"
-(New-Object System.Net.WebClient).DownloadFile($url, $outpath)
-Add-AppxPackage $outpath
+#$url = "https://github.com/microsoft/winget-cli/releases/download/v1.6.3482/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+#$outpath = "winget.msixbundle"
+#(New-Object System.Net.WebClient).DownloadFile($url, $outpath)
+#Add-AppxPackage $outpath
 
 # Install Powershell 7
-winget install --id Microsoft.Powershell --source winget
+#winget install --id Microsoft.Powershell --source winget
+
+
+# Install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install Powershell 7 via choco
+choco install powershell-core --force --confirm
+
+# Install winget via choco
+choco install winget --force --confirm
 
 # Install Powershell 7 modules
 Set-ExecutionPolicy Bypass -Scope Process -Force;
@@ -19,9 +29,6 @@ $pwsh_path = "C:\Program Files\PowerShell\7\pwsh.exe"
 & "$pwsh_path" -c 'Install-Module -Name posh-git -Confirm:$False -Force'
 & "$pwsh_path" -c 'Install-Module -Name PSFzf -Confirm:$False -Force'
 & "$pwsh_path" -c 'Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(''https://ohmyposh.dev/install.ps1''))'
-
-# Install choco
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install chezmoi via choco
 choco install chezmoi --force --confirm
