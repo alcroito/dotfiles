ed8c07b73fc5033975300f28abd98354fe8ac9d6
alcroito <alcroito.code@pm.me>
2025-03-10 18:39:06 +0100

    Port which-key to lazy

---
+++ home/dot_config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,14 @@
+return {
+  -- vim keybindings popup guide
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
