656e7351817c5ab17d297826d859a334e0b6a1b3
alcroito <alcroito.code@pm.me>
2025-06-07 15:27:41 +0200

    Replace telescope with snacks

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+if true then return {} end
+
 local plugin = {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
@@ -38,27 +40,27 @@ local plugin = {
         }
       }
     }}
-    telescope.load_extension("json_history")
+    --telescope.load_extension("json_history")
     -- Load support specifying live grep args
-    telescope.load_extension("live_grep_args")
+    --telescope.load_extension("live_grep_args")
     -- Load telescope recent files and set binding
-    telescope.load_extension("recent_files")
+    --telescope.load_extension("recent_files")
     -- Telescope fzf c faster impl
-    if vim.fn.executable("cmake") and vim.fn.executable("ninja") then
-      telescope.load_extension("fzf")
-    end
+    --if vim.fn.executable("cmake") and vim.fn.executable("ninja") then
+      --telescope.load_extension("fzf")
+    --end
   end,
   keys = {
     --" Find files using Telescope command-line sugar.
-    {"<leader>ff", function() require("telescope.builtin").find_files() end, desc = "files",},
-    {"<leader>fg", function() require("telescope").extensions.live_grep_args.live_grep_args() end, desc = "live grep args", },
+    --{"<leader>ff", function() require("telescope.builtin").find_files() end, desc = "files",},
+    --{"<leader>fg", function() require("telescope").extensions.live_grep_args.live_grep_args() end, desc = "live grep args", },
     {"<leader>fs", "<cmd>:execute 'Telescope live_grep_args default_text=' . expand('<cword>')<cr>", desc = "grep current word"},
-    {"<leader>fb", function() require("telescope.builtin").buffers() end, desc = "buffers", },
+    --{"<leader>fb", function() require("telescope.builtin").buffers() end, desc = "buffers", },
     {"<leader>fh", function() require("telescope.builtin").help_tags() end, desc = "help_tags", },
     {"<leader>fe", function() require("telescope.builtin").treesitter() end, desc = "treesitter", },
-    {"<leader>fc", function() require("telescope.builtin").current_buffer_fuzzy_find() end, desc = "current buffer fuzzy find", },
+    --{"<leader>fc", function() require("telescope.builtin").current_buffer_fuzzy_find() end, desc = "current buffer fuzzy find", },
     {"<leader>ft", function() require("telescope.builtin").builtin() end, desc = "builtin", },
-    {"<leader>r", "<cmd>lua require('telescope').extensions.recent_files.pick()<cr>", desc = "recent files"},
+    --{"<leader>r", "<cmd>lua require('telescope').extensions.recent_files.pick()<cr>", desc = "recent files"},
     -- Bind <leader>; to reopen latest telescope window
     {"<leader>;", function() require("telescope.builtin").resume(require('telescope.themes').get_ivy({})) end, desc = "resume telescope"},
   },
