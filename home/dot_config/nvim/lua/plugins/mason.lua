db093793cc1c67b471e5ff402f6f7722141ca46b
alcroito <alcroito.code@pm.me>
2025-06-09 16:11:41 +0200

    Reformat mason lua file

--- home/dot_config/nvim/lua/plugins/mason.lua
+++ home/dot_config/nvim/lua/plugins/mason.lua
@@ -10,68 +10,68 @@ local plugin = {
   },
   config = function()
     require("mason").setup()
-    require'mason-lspconfig'.setup{
-      ensure_installed = {"harper_ls", "tinymist"}
-    }
+    require("mason-lspconfig").setup({
+      ensure_installed = { "harper_ls", "tinymist" },
+    })
 
     require("mason-null-ls").setup({
-      ensure_installed = { "stylua" }
+      ensure_installed = { "stylua" },
     })
 
     local nvim_lsp = require("lspconfig")
-    nvim_lsp.neocmake.setup{
-    cmd = { "neocmakelsp", "--stdio" },
+    nvim_lsp.neocmake.setup({
+      cmd = { "neocmakelsp", "--stdio" },
       filetypes = { "cmake" },
       root_dir = function(fname)
-          return nvim_lsp.util.find_git_ancestor(fname)
+        return nvim_lsp.util.find_git_ancestor(fname)
       end,
-      single_file_support = true,-- suggested
+      single_file_support = true, -- suggested
       init_options = {
-          format = {
-              enable = false
-          },
-          lint = {
-              enable = false
-          },
-          scan_cmake_in_package = true -- default is true
-      }
-    }
+        format = {
+          enable = false,
+        },
+        lint = {
+          enable = false,
+        },
+        scan_cmake_in_package = true, -- default is true
+      },
+    })
 
     -- neovim 0.11+ syntax
-    vim.lsp.config('harper_ls', {
-      filetypes = {"gitcommit", "git-commit", "markdown", "cmake"},
+    vim.lsp.config("harper_ls", {
+      filetypes = { "gitcommit", "git-commit", "markdown", "cmake" },
       settings = {
         ["harper-ls"] = {
-          userDictPath = vim.fn.expand('$HOME/.vim/spell/en.utf-8.add'),
+          userDictPath = vim.fn.expand("$HOME/.vim/spell/en.utf-8.add"),
           linters = {
-              spell_check = true,
-              spelled_numbers = false,
-              an_a = true,
-              sentence_capitalization = true,
-              unclosed_quotes = true,
-              wrong_quotes = false,
-              long_sentences = true,
-              repeated_words = true,
-              spaces = true,
-              matcher = true,
-              correct_number_suffix = true,
-              number_suffix_capitalization = true,
-              multiple_sequential_pronouns = true,
-              linking_verbs = false,
-              avoid_curses = true,
-              terminating_conjunctions = true
+            spell_check = true,
+            spelled_numbers = false,
+            an_a = true,
+            sentence_capitalization = true,
+            unclosed_quotes = true,
+            wrong_quotes = false,
+            long_sentences = true,
+            repeated_words = true,
+            spaces = true,
+            matcher = true,
+            correct_number_suffix = true,
+            number_suffix_capitalization = true,
+            multiple_sequential_pronouns = true,
+            linking_verbs = false,
+            avoid_curses = true,
+            terminating_conjunctions = true,
           },
           diagnosticSeverity = "hint",
-        }
-      }
+        },
+      },
     })
-    require'lspconfig'.tinymist.setup {
+    require("lspconfig").tinymist.setup({
       settings = {
         formatterMode = "typstyle",
         exportPdf = "onType",
-        semanticTokens = "disable"
-      }
-    }
+        semanticTokens = "disable",
+      },
+    })
     -- Needed to load the LSPs for some reason.
     -- https://old.reddit.com/r/neovim/comments/14cikep/on_nightly_my_lsp_is_not_starting_automatically/
     --vim.api.nvim_exec_autocmds("FileType", {})
