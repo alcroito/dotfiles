a431f1731e179d0fb3eb32594c1360ad46c75038
alcroito <alcroito.code@pm.me>
2025-06-09 16:10:21 +0200

    Add confirm neovim plugin to format lua files

---
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+  },
+}
