02ab5a1ea8ac6b21280db2bb5d4e2277a980cd02
alcroito <alcroito.code@pm.me>
2025-07-29 17:40:16 +0200

    Set up familiar hotkey for git blame in Neovim
    Use , and . to navigate the blame stack, and i to show commit info.
    The first two are similar to tig hotkeys.
    Also make the loading of the plugin lazy.

--- home/dot_config/nvim/lua/plugins/blame.lua
+++ home/dot_config/nvim/lua/plugins/blame.lua
@@ -1,9 +1,14 @@
 return {
   "alcroito/blame.nvim",
-  lazy = false,
-  config = function()
-    require('blame').setup {}
-  end,
+  opts = {
+    mappings = {
+        commit_info = "i",
+        stack_push = ",",
+        stack_pop = ".",
+        show_commit = "<CR>",
+        close = { "<esc>", "q" },
+    },
+  },
   keys = {
     { "<leader>b", mode = { "n", "x", "o" }, "<cmd>BlameToggle<CR>", desc = "Git blame" },
   },
