f38041f3b92b6d075dd457dd474c25100b2fe243
alcroito <alcroito.code@pm.me>
2025-10-29 22:51:52 +0100

    nvim: Add some disabled blink auto complete copilot stuff
    Disabled because blink doesn't support multi line copilot shadow
    diffs.

--- home/dot_config/nvim/lua/plugins/blink.lua
+++ home/dot_config/nvim/lua/plugins/blink.lua
@@ -1,14 +1,15 @@
 if true then return {} end
 
-return
-
-{
-  'saghen/blink.cmp',
+return {
+  "saghen/blink.cmp",
   -- optional: provides snippets for the snippet source
-  --dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "fang2hou/blink-copilot",
+  },
 
   -- use a release tag to download pre-built binaries
-  version = '1.*',
+  version = "1.*",
   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
@@ -29,21 +30,32 @@ return
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    keymap = { preset = "super-tab" },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
+      nerd_font_variant = "mono",
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = { documentation = { auto_show = false }, menu = {
+      auto_show = false,
+    } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { "copilot", "lsp" },
+      --default = { "copilot", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-copilot",
+          score_offset = 100,
+          async = true,
+        },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
@@ -51,7 +63,7 @@ return
     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
     --
     -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    fuzzy = { implementation = "prefer_rust_with_warning" },
   },
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
 }
